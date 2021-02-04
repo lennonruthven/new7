3227cd7a9bdd71c5697cf45db39581b411d59bb7
lennonruthven <77502499+lennonruthven@users.noreply.github.com>
2021-02-04 02:43:36 +0000

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+import ExtLink from './ext-link'
+
+export default () => (
+  <>
+    <footer>
+      <span>...</span>
+      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
+        <img
+          src=""
+          height={46}
+          width={132}
+          alt=".."
+        />
+      </ExtLink>
+      <span>
+        {' '}
+        <ExtLink href="">
+        
+        </ExtLink>
+      </span>
+    </footer>
+  </>
+)
